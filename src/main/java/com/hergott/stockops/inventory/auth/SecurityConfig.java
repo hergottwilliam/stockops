e1537d6d1f78c612b8df2197c3d5745a5a5fb4d5
hergottwilliam <hergottwilliam@gmail.com>
2023-11-26 16:35:32 -0500

    oauth2 authentication works with google, but does not send user to app

---
+++ src/main/java/com/hergott/stockops/inventory/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.hergott.stockops.inventory.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.security.auth.message.config.AuthConfig;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeHttpRequests( auth -> {
+					auth.requestMatchers("/").permitAll();
+					auth.anyRequest().authenticated();
+				})
+				.oauth2Login(Customizer.withDefaults())
+				.formLogin(Customizer.withDefaults())
+				.build();
+	}
+}
--- src/main/java/com/hergott/stockops/inventory/config/DatabaseConnector.java
+++ src/main/java/com/hergott/stockops/inventory/config/DatabaseConnector.java
@@ -4,7 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseConnector {
+	
+	@Bean
 	public static Connection getConnection() throws SQLException {
 		String url = "jdbc:mysql://localhost:3306/stockops_db";
 		String username = "stockopsuser";
